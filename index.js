4e92dfac0abb97fd69076e08cad5f25d5f91de5f
flan the dev <flanthedev@gmail.com>
2017-08-25 19:43:08 +0800

    only new coordinates added to solution array

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const containerElement = document.getElementById('container');
 let playerTurn = 1;
 const board = [ [], [], [], [], [], [], [] ];
 const player1 = [];
+const player1win = [];
 const player2 = [];
+const player2win = [];
+let win = false;
 
 const game = {
 	render: function() {
@@ -35,10 +38,10 @@ const game = {
 		game.update(currentPlayer);
 		playerTurn === 1 ? playerTurn = 2 : playerTurn = 1;
 
-		console.log(`clicked on column ${colNum} : coordinate ${coordinate}`);
-		console.log(`p1 has : ${player1}`);
-		console.log(`p2 has : ${player2}`);
-		console.log(`column ${colNum} is now ${board[colNum]}`);
+		// console.log(`clicked on column ${colNum} : coordinate ${coordinate}`);
+		// console.log(`p1 has : ${player1}`);
+		// console.log(`p2 has : ${player2}`);
+		// console.log(`column ${colNum} is now ${board[colNum]}`);
 	},
 
 	update: function(currentPlayer) {
@@ -46,8 +49,8 @@ const game = {
 			let divId = currentPlayer[i].toString();
 			let div = document.getElementById(divId);
 			div.className += ` player${playerTurn}`;
-			game.checkWin(currentPlayer, i);
 		}
+		game.checkWin(currentPlayer);
 	},
 
 	searchArrayForItem: function(array, item) {
@@ -59,16 +62,44 @@ const game = {
 		return false; // item not found in array
 	},
 
-	checkWin: function(currentPlayer, i) {
-		let above1 = [currentPlayer[i][0], currentPlayer[i][1] + 1];
-		let above2 = [currentPlayer[i][0], currentPlayer[i][1] + 2];
-		let above3 = [currentPlayer[i][0], currentPlayer[i][1] + 3];
-		
-		if (game.searchArrayForItem(currentPlayer, above1) && 
-			game.searchArrayForItem(currentPlayer, above2) && 
-			game.searchArrayForItem(currentPlayer, above3)) {
-			console.log('-------------win------------');
+	compareArrays: function(winArray, currentPlayer) {
+		for (let i = 0; i < currentPlayer.length; i++) {
+			let item = currentPlayer[i];
+
+			if (game.searchArrayForItem(winArray, item)) {
+				console.log('match');
+				return true;
+			} else {
+				return false;
+			}
 		}
+		// return false;
+	},
+
+	checkWin: function(currentPlayer) {
+		if (playerTurn === 1 ) {
+			for (let i = 0; i < currentPlayer.length; i++) {
+				for (let j = 1; j <= 3; j++) {
+					let solution = [currentPlayer[i][0], currentPlayer[i][1] + j];
+					if (!game.searchArrayForItem(player1win, solution)) {
+						player1win.push(solution);
+					}
+				}			
+			}
+
+			console.log(player1win);			
+		}
+
+
+		// if (game.compareArrays(vertical, currentPlayer)) {
+		// 	console.log('-----------win-----------');
+		// }
+		
+		// if (game.searchArrayForItem(currentPlayer, vertical[0]) && 
+		// 	game.searchArrayForItem(currentPlayer, vertical[1]) && 
+		// 	game.searchArrayForItem(currentPlayer, vertical[2])) {
+		// 	console.log('-------------win------------');
+		// }
 	}
 
 }
