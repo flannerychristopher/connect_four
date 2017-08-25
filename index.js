52aae8fd25302cd7ed0dc4da2d23489b04c866c9
flan the dev <flanthedev@gmail.com>
2017-08-25 11:59:09 +0800

    create board array, change box id

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const containerElement = document.getElementById('container');
 
 const game = {
 	playerTurn: 1,
+	board: [],
 	player1: [],
 	player2: [],
 
@@ -13,17 +14,20 @@ const game = {
 			div.addEventListener('click', this.handler, false);
 			containerElement.appendChild(div);
 		}
-		for (i = 0; i < 42; i++) {
-			let div = document.createElement('div');
-			div.className = 'box';
-			div.id = `box${i}`;
-			containerElement.appendChild(div);
+		for (i = 0; i < 6; i++) {
+			for (j = 0; j < 7; j++) {
+				let div = document.createElement('div');
+				div.className = 'box';
+				div.id = `${i}-${j}`;
+				containerElement.appendChild(div);
+				game.board.push([i, j]);
+			}
 		}
 	},
 
 	handler: function() {
 		console.log('test');
 	}
-
+	
 }
 game.render();
